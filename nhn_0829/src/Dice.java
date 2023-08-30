b824029ecc45d00843aa177b51c92322eb72cb76
DaeYoul-Yang <cnrrnehfdl03@naver.com>
2023-08-30 10:45:18 +0900

    second

--- nhn_0829/src/Dice.java
+++ nhn_0829/src/Dice.java
@@ -7,5 +7,6 @@ public class Dice {
         return number;
     }
 
+
     private int number;
 }
--- nhn_0829/src/Library.java
+++ nhn_0829/src/Library.java
@@ -1,41 +1,44 @@
-import java.util.Scanner;
-
 public class Library {
 
     private String[] list;
 
-    private  int count;
+    private int count;
 
     public Library(int size) {
-        if (size <= 0) throw new IllegalArgumentException("음수는 불가능");
+        if (size <= 0) {
+            throw new IllegalArgumentException("음수는 불가능");
+        }
         list = new String[size];
         for (int i = 0; i < list.length; i++) {
-            list[i]="";
+            list[i] = "";
 
         }
-        count =0;
+        count = 0;
 
     }
 
     public void add(String s) {
 
-        if(count == list.length)
+        if (count == list.length) {
             throw new IllegalArgumentException("책장이 꽉 찼습니다");
+        }
 
         for (int i = 0; i < list.length; i++) {
-            if(list[i].equals(s))
-                throw new IllegalArgumentException("["+s+"]은 이미 있는책");
+            if (list[i].equals(s)) {
+                throw new IllegalArgumentException("[" + s + "]은 이미 있는책");
+            }
 
         }
 
-        list[count]=s;
-        count+=1;
+        list[count] = s;
+        count += 1;
 
     }
 
     public void delete(String s) {
-        if (count == 0)
+        if (count == 0) {
             throw new IllegalArgumentException("책장이 비어있어서 삭제 불가능");
+        }
         for (int i = 0; i < list.length; i++) {
             if (list[i].equals(s)) {
                 for (int j = i; j < list.length - 1; j++) {
@@ -51,9 +54,6 @@ public class Library {
     }
 
 
-
-
-
     public void find(String s) {
         for (int i = 0; i < list.length; i++) {
             if (list[i].equals(s)) {
@@ -62,10 +62,10 @@ public class Library {
             }
         }
 
-        System.out.println(s+"없는책입니다.");
+        System.out.println(s + "없는책입니다.");
 
 
-        }
+    }
 
 
     public void findAll() {
@@ -73,9 +73,10 @@ public class Library {
         System.out.println("============");
 
         for (int i = 0; i < list.length; i++) {
-            if(list[i].equals(""))
+            if (list[i].equals("")) {
                 continue;
-            System.out.println((i+1)+" "+list[i]);
+            }
+            System.out.println((i + 1) + " " + list[i]);
 
         }
 
